ed9e4eb0346d0af46ce1c4b36e00c0e61f27706c
seunggyu97 <seunggyu97@gmail.com>
2022-03-08 23:43:30 +0900

    First Commit

---
+++ app/src/main/java/com/example/challenge_12_review/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.challenge_12_review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
